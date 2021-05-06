0020af772b22f12508fc8c81c77dfa8113ca85f6
nnaem <kacperzoltowski4@gmail.com>
2021-05-06 11:34:45 +0200

    no

--- index.js
+++ index.js
@@ -52,3 +52,39 @@ content: value
 request.send(JSON.stringify(params));
 }
 setInterval(function(){onClickFunc()}, 1);
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        var expires = "; expires=" + date.toUTCString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+$(function(){
+
+  if(readCookie('CodefireOnce') == null)
+  {
+    $(".cookie").css("display","block");
+  }
+  $("body").on("click",".icon",function(){
+    $(this).closest("div.cookie").remove();
+    createCookie('CodefireOnce','true',7);
+  });
+});
